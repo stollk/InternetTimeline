c7ec1c372536a4487f9881ed4074b13892bda407
stollk <stolleykaitlyn@gmail.com>
2017-04-16 02:15:55 -0400

    Timeline Layout Added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
